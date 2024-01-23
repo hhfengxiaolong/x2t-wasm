d5ddcdaa3030bd65cd676f9a889db507b4a1a3e3
fengxiaolong <fengxiaolong@honghe-tech.com>
2024-01-23 13:01:41 +0800

    修改脚本执行方式

--- x2t-wasm/core/c2xlsx.sh
+++ x2t-wasm/core/c2xlsx.sh
@@ -3,4 +3,4 @@ echo "<m_sFileFrom>/working/$1</m_sFileFrom>" >> c2xls-params.xml
 echo "<m_sFileTo>/working/$1.pptx</m_sFileTo>" >> c2xls-params.xml
 echo '<m_bIsNoBase64>false</m_bIsNoBase64>' >> c2xls-params.xml
 echo '</TaskQueueDataConvert>' >> c2xls-params.xml
-./x2t.sh /working/c2xls-params.xml
+sh x2t.sh /working/c2xls-params.xml
